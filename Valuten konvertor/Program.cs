67050d2d2c49aceed82829bd852cf3b6ae0d546e
preslav95 <preslav95@mail.bg>
2016-09-20 15:11:19 +0300

    Add files via upload

---
+++ Valuten konvertor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valuten_konvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Currency = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
